e2b4d6dc9cab2ef4abc67687a34bd73831e6edc5
Maeelia <103173774+Maeelia@users.noreply.github.com>
2022-04-12 13:50:15 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,99 @@
+basic.forever(function () {
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+    basic.pause(100)
+})
